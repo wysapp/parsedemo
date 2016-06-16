66a6d73627eb53b75578e789c0e74d0b50ad6a1a
os x <osx@osdeiMac.local>
2016-06-16 16:35:54 +0800

    demo-parse-dashboard

---
+++ parse-dashboard/src/dashboard/Dashboard.js
@@ -0,0 +1,126 @@
+
+import Parse from 'parse';
+import ParseApp from 'lib/ParseApp';
+import React from 'react';
+
+import Loader from 'components/Loader/Loader.react';
+
+
+import {AsyncStatus} from 'lib/Constants';
+import { center } from 'stylesheets/base.scss';
+import {get} from 'lib/AJAX';
+import { setBasePath } from 'lib/AJAX';
+
+import {
+  Router,
+  Route,
+  Redirect
+} from 'react-router';
+
+
+let App = React.createClass({
+  render() {
+    return this.props.children;
+  }
+});
+
+let Empty = React.createClass({
+  render() {
+    return <div>Not yet implemented</div>;
+  }
+});
+
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      configLoadingError: '',
+      configLoadingState: AsyncStatus.PROGRESS,
+      newFeaturesInLatestVersion: [],
+    };
+    setBasePath(props.path);
+  }
+  
+  componentDidMount() {
+    get('/parse-dashboard-config.json').then(({ apps, newFeaturesInLatestVersion = [] }) => {
+      this.setState({ newFeaturesInLatestVersion });
+      console.log(apps);
+      let appInfoPromises = apps.map(app => {
+        if (app.serverURL.startsWith('https://api.parse.com/1')) {
+          //api.parse.com doesn't have feature availability endpoint, fortunately we know which features
+          //it supports and can hard code them
+          app.serverInfo = PARSE_DOT_COM_SERVER_INFO;
+          return Parse.Promise.as(app);
+        } else {
+          app.serverInfo = {};
+          
+                    
+          return new ParseApp(app).apiRequest(
+            'GET',
+            'serverInfo',
+            {},
+            { useMasterKey: true }
+          ).then(serverInfo => {
+            app.serverInfo = serverInfo;
+            return app;
+          }, error => {
+            if (error.code === 100) {
+              app.serverInfo = {
+                error: 'unable to connect to server',
+                enabledFeatures: {},
+                parseServerVersion: "unknown"
+              }
+              return Parse.Promise.as(app);
+            } else if (error.code === 107) {
+              app.serverInfo = {
+                error: 'server version too low',
+                enabledFeatures: {},
+                parseServerVersion: "unknown"
+              }
+              return Parse.Promise.as(app);
+            } else {
+              app.serverInfo = {
+                error: error.message || 'unknown error',
+                enabledFeatures: {},
+                parseServerVersion: "unknown"
+              }
+              return Parse.Promise.as(app);
+            }
+          });
+        }
+      });
+      return Parse.Promise.when(appInfoPromises);
+    }).then(function() {
+      Array.prototype.slice.call(arguments).forEach(app => {
+        AppsManager.addApp(app);
+      });
+      this.setState({ configLoadingState: AsyncStatus.SUCCESS });
+    }.bind(this)).fail(({ error }) => {
+      this.setState({
+        configLoadingError: error,
+        configLoadingState: AsyncStatus.FAILED
+      });
+    });
+  }
+  
+  
+  render() {
+    if ( this.state.configLoadingState === AsyncStatus.PROGRESS) {
+      return <div className={center}><Loader /></div>;
+    }
+    
+    if ( this.state.configLoadingError && this.state.configLoadingError.length > 0) {
+      return <div className={styles.empty}>
+          <div className={center}>
+            <div className={styles.cloud}>
+              <Icon width={110} height={110} name='cloud-surprise' fill='#1e3b4d' />
+            </div>
+            <div className={styles.loadingError}>{this.state.configLoadingError.replace(/-/g, '\u2011')}</div>
+          </div>
+        </div>
+    }
+  }
+}
+
+module.exports = Dashboard;
---
+++ parse-dashboard/src/lib/AppsManager.js
@@ -0,0 +1,11 @@
+import Parse from 'parse';
+import { get, post, del } from 'lib/AJAX';
+import { unescape } from 'lib/StringEscaping';
+
+let appsStore = [];
+
+const AppsManager = {
+  
+}
+
+export default AppsManager;
